e966ec77378338260cf3f9d4c3e08e833550aa95
José Galisteo <ceritium@gmail.com>
2024-05-15 22:05:37 +0200

    Fix sync branch job

--- app/jobs/sync_branch_job.rb
+++ app/jobs/sync_branch_job.rb
@@ -4,17 +4,13 @@ class SyncBranchJob < ApplicationJob
   queue_as :default
 
   def perform(project, branch_name)
-    update_branch_status(project, { name: branch_name, sync_status: "in_progress" })
+    branch = project.branches.find_or_create_by!(name: branch_name)
+    branch.update!(sync_status: "in_progress")
 
     FetchBranchTranslations.new(project, branch_name:).fetch!
-
-    update_branch_status(project, { name: branch_name, sync_status: "done", synced_at: Time.current })
+    branch.update!(sync_status: "done", synced_at: Time.current)
   rescue StandardError => e
-    update_branch_status(project, { name: branch_name, sync_status: "failed" })
+    branch.update!(sync_status: "failed")
     raise e
-  end
-
-  def update_branch_status(project, attrs)
-    project.branches.upsert(attrs, unique_by: %i[name project_id])
   end
 end
