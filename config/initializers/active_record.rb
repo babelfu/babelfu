a69dfe9e3849c0f53a1902c49bd7232270c4dbe4
José Galisteo <ceritium@gmail.com>
2024-05-15 07:13:58 +0200

    Fix ActiveRecord encryption configuration

--- config/initializers/active_record.rb
+++ config/initializers/active_record.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
-Rails.application.config.active_record.encryption do |c|
-  c.primary_key = Babelfu.config.active_record_encryption_primary_key
-  c.deterministic_key = Babelfu.config.active_record_encryption_deterministic_key
-  c.key_derivation_salt = Babelfu.config.active_record_encryption_key_derivation_salt
+require Rails.root.join('lib/babelfu')
+
+Rails.application.configure do |c|
+  c.config.active_record.encryption.primary_key = Babelfu.config.active_record_encryption_primary_key
+  c.config.active_record.encryption.deterministic_key = Babelfu.config.active_record_encryption_deterministic_key
+  c.config.active_record.encryption.key_derivation_salt = Babelfu.config.active_record_encryption_key_derivation_salt
 end
--- lib/babelfu.rb
+++ lib/babelfu.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-
+require 'babelfu/config'
 module Babelfu
   class << self
     def config
